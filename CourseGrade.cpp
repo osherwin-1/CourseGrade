a89327b9d2f5781f1f921a43f533e678650ea55a
osherwin-1 <olive@DESKTOP-7OL15UE>
2026-03-03 16:49:59 -0500

    Added comments for clarity + fixed syntax for clarity

--- CourseGrade.cpp
+++ CourseGrade.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #include <iostream>
 #include <fstream>
 #include <string>
-
+// Structure holding student information
 struct Student
 {
 	string name;
@@ -13,7 +13,10 @@ struct Student
 	double averageScore;
 	char letterGrade;
 };
-
+// Function to read student data from a file
+// Takes student array, number of students, and number of tests as parameters.
+// Precondition: The file "student_data.txt" must exist and be formatted correctly with the number of students, number of tests, and each student's name, ID, and test scores. Postcondition: The student array is populated with data from the file.
+// Postcondition: The student array is populated with data from the file.
 void readData(Student*& students, int& numStudents, int& numTests)
 {
 	
@@ -38,7 +41,10 @@ void readData(Student*& students, int& numStudents, int& numTests)
 	}
 	inFile.close();
 }
-
+// Function to calculate average scores for each student
+// Takes student array, number of students, and number of tests as parameters. 
+// Precondition: The student array must be populated with test scores. 
+// Postcondition: Each student's average score is calculated and stored in the averageScore member of the Student struct.
 void calculateAverages(Student* students, int numStudents, int numTests)
 {
 	for (int i = 0; i < numStudents; i++)
@@ -51,7 +57,10 @@ void calculateAverages(Student* students, int numStudents, int numTests)
 		students[i].averageScore = total / numTests;
 	}
 }
-
+// Function to assign letter grades based on average scores
+// Takes student array and number of students as parameters.
+// Precondition: Each student's average score must be calculated and stored in the averageScore member of the Student struct.
+// Postcondition: Each student's letter grade is assigned based on their average score and stored in the letterGrade member of the Student struct.
 void giveLetter(Student* students, int numStudents)
 {
 	for (int i = 0; i < numStudents; i++)
@@ -68,10 +77,14 @@ void giveLetter(Student* students, int numStudents)
 			students[i].letterGrade = 'F';
 	}
 }
-
+// Function to print the course report card
+// Takes student array, number of students, and number of tests as parameters.
+// Precondition: Each student's name, ID, average score, and letter grade must be calculated and stored in the respective members of the Student struct.
+// Postcondition: The course report card is printed to the console, displaying each student's name, ID, average score, and letter grade in a formatted manner.
 void printReportCard(const Student* students, int numStudents, int numTests)
 {
 	cout << "Course Report Card" << endl;
+	cout << "" << endl;
 	cout << "Name\tID\tAverage\tGrade" << endl;
 	cout << "-------------------------------" << endl;
 	for (int i = 0; i < numStudents; i++)
@@ -92,10 +105,10 @@ int main()
 	giveLetter(students, numStudents);
 	printReportCard(students, numStudents, numTests);
 	
-	for (int i = 0; i < numStudents; i++) 
+	for (int i = 0; i < numStudents; i++) //Removes each element of the testScores array for each student to prevent memory leaks.
 	{
 		delete[] students[i].testScores;
 	}
-	delete[] students;
-	students = nullptr;
+	delete[] students; // Removes the students array to prevent memory leaks.
+	students = nullptr; // Sets the students pointer to nullptr after deleting to avoid pointer issues.
 }
